21801395f6ae8f239340c42bbca4e89406634408
anhduc2337 <anhduc2337@gmail.com>
2023-11-07 08:03:46 +0700

    main

--- src/main/java/swp/studentprojectportal/utils/JwtTokenProvider.java
+++ src/main/java/swp/studentprojectportal/utils/JwtTokenProvider.java
@@ -1,6 +1,7 @@
 package swp.studentprojectportal.utils;
 
-import io.jsonwebtoken.Claims;
+import io.jsonwebtoken
+        .Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
